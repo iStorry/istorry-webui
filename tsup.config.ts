074e145e01d0d2c60a0bc921e2deade1c524cbd4
Jatinjit Singh <jatinx.lol@gmail.com>
2024-06-25 09:46:23 +0900

    updated the action.yml and tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,32 +1,26 @@
 import { defineConfig, type Options } from "tsup";
+import path from "path";
 
 const env = process.env.NODE_ENV;
-import path from "path";
 
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   target: "es2020",
   format: ["cjs", "esm"], // generate cjs and esm files
-  entry: [
-    "./src/components/ui/!(index).ts?(x)",
-    "./src/lib/!(index).ts?(x)",
-    // "!./**/*.spec.*",
-    // "!./**/*.stories.*",
-  ],
+  entry: ["./src/components/ui/**/*.{ts,tsx}", "./src/lib/**/*.{ts,tsx}"],
   skipNodeModulesBundle: true, // Skips building dependencies for node modules
   minify: !options.watch && env === "production",
-  bundle: false, //env === 'production',
+  bundle: false,
   clean: true, // clean up the dist folder
   dts: true, // generate dts file for main module
-  sourcemap: true, //env === 'production', // source map is only available in prod
-  outDir: "dist", // env === 'production' ? 'dist' : 'lib',
+  sourcemap: env === "production", // source map is only available in prod
+  outDir: "dist",
   tsconfig: path.resolve(__dirname, "./tsconfig.json"),
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   esbuildOptions(options, context) {
-    options.outbase = "./";
+    options.outbase = "src";
   },
   external: ["react"],
   ...options,
-  // banner: {js: '"use client";'},
+  // button: { js: '"use client";' },
 }));
