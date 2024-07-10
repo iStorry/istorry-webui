17f95c7297ba6f3b00aa0141a8684eae9791c5d5
Jatinjit Singh <jatinx.lol@gmail.com>
2024-07-10 12:16:59 +0900

    update bento grid

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -40,19 +40,21 @@ export const BentoGridItem = ({
       )}
     >
       {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        {title && (
-          <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-            {title}
-          </div>
-        )}
-        {description && (
-          <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-            {description}
-          </div>
-        )}
-      </div>
+      {title || description ? (
+        <div className="group-hover/bento:translate-x-2 transition duration-200">
+          {icon}
+          {title && (
+            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+              {title}
+            </div>
+          )}
+          {description && (
+            <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+              {description}
+            </div>
+          )}
+        </div>
+      ) : null}
     </div>
   );
 };
