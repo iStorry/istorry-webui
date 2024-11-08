b7a1e402e3ce51c25ffb83d19a6d0286c272f0da
Jatinjit Singh <jatinx.lol@gmail.com>
2024-11-08 09:21:34 +0900

    fix password icon

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -28,9 +28,9 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
           disabled={props.value === "" || props.disabled}
         >
           {showPassword ? (
-            <EyeClosedIcon className="h-4 w-4" aria-hidden="true" />
-          ) : (
             <EyeOpenIcon className="h-4 w-4" aria-hidden="true" />
+          ) : (
+            <EyeClosedIcon className="h-4 w-4" aria-hidden="true" />
           )}
           <span className="sr-only">
             {showPassword ? "Hide password" : "Show password"}
